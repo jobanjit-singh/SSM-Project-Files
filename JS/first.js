f05840ab2030cb960d301f4df5e8d4b45a50a92e
jobanjit-singh <singhjobanjit02@gmail.com>
2025-02-10 14:38:11 +0530

    js started

---
+++ JS/first.js
@@ -0,0 +1,1 @@
+console.log("Sat Sri Akal Ji...");
---
+++ Projects/Project1/script.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+
+var a = 10;
+let b = 20;
+
+const name = "Jobanjit Singh";
+
+// console.log(a);
+// console.log(b);
+// console.log(name);
+
+a = 20;
+b = 30;
+
+// name = "Singh";
+
+console.log(`A = ${a}\nB = ${b}\nName = ${name}`);
+// console.log(b);
+// console.log(name);
+
+// console.table([a, b, name]);
